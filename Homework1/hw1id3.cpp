27576af8195c18e10449d38942df9d2f3f533453
Jakob Snyder <jsnyder20@unl.edu>
2016-09-20 12:55:04 -0500

    adding structs

--- Homework1/hw1id3.cpp
+++ Homework1/hw1id3.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 
+struct monks_t{
+  int clas;
+  int a1;
+  int a2;
+  int a3;
+  int a4;
+  int a5;
+  int a6;
+  int id;
+}
+
+struct poker_t{
+  int clas;
+  int s1;
+  int c1;
+  int s2;
+  int c2;
+  int s3;
+  int c3;
+  int s4;
+  int c4;
+  int s5;
+  int c5;
+}
+
+struct votes_t{
+  int clas;
+  int handicapp;
+  int water;
+  int adopt;
+  int physician;
+  int elSalvador;
+  int religious;
+  int antiSat;
+  int aid;
+  int mx;
+  int immig;
+  int synfuels;
+  int education;
+  int superfund;
+  int crime;
+  int duty;
+  int exportAdmin;
+}
+
 
 int main()
 {
-  std::cout << "Hello World!";
+  cout << "Hello World!";
 }
