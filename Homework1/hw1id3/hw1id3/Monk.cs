696cf86c413cd7e359572be5493424d210a2fe5f
Jakob Snyder <jsnyder20@unl.edu>
2016-09-20 14:42:23 -0500

    changing to c# because its bae

---
+++ Homework1/hw1id3/hw1id3/Monk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1id3
+{
+    class Monk
+    {
+        public int clas;
+        public int a1;
+        public int a2;
+        public int a3;
+        public int a4;
+        public int a5;
+        public int a6;
+        public int id;
+        public const int numClasses = 8;
+    }
+}
---
+++ Homework1/hw1id3/hw1id3/Poker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1id3
+{
+    class Poker
+    {
+        public int clas;
+        public int s1;
+        public int c1;
+        public int s2;
+        public int c2;
+        public int s3;
+        public int c3;
+        public int s4;
+        public int c4;
+        public int s5;
+        public int c5;
+        public const int numClasses = 11;
+    }
+}
