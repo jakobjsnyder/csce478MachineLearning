7acb11c34ff6027d5783e1617a149d9dc76ebf8b
Jakob Snyder <jsnyder20@unl.edu>
2016-10-11 21:26:16 -0500

    adding homework 2 project

---
+++ Homework2/hw2ann/hw2ann/Parser.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using hw2ann;
+
+// created by Jakob Snyder(jsnyde) and Clayton Henderson(chenders)
+
+namespace hw2ann
+{
+    class Parser
+    {
+        private static List<Element> parseMonk()
+        {
+            List<Element> fullData = new List<Element>();
+            string line;
+
+            // Read the file and display it line by line.
+            StreamReader file = new StreamReader("monks.data");
+            while ((line = file.ReadLine()) != null)
+            {
+                int clas;
+                List<int> attri = new List<int>();
+                line = line.Trim();
+                var split = line.Split(' ');
+                clas = Convert.ToInt32(split.First());
+                int i = 0;
+                foreach (var item in split)
+                {
+                    if (i == 0)
+                    {
+                        i++;
+                    }
+                    else if (i == 7)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        attri.Add(Convert.ToInt32(item));
+                        i++;
+                    }
+                }
+                fullData.Add(new Element(clas, attri));
+            }
+
+            file.Close();
+            return fullData;
+        }
+
+        private static List<Element> parsePoker()
+        {
+            List<Element> fullData = new List<Element>();
+            string line;
+
+            // Read the file and display it line by line.
+            StreamReader file = new StreamReader("poker.data");
+            while ((line = file.ReadLine()) != null)
+            {
+                int clas;
+                List<int> attri = new List<int>();
+                line = line.Trim();
+                var split = line.Split(',');
+                clas = (Convert.ToInt32(split.Last()) <= 4) ? 0 : 1;// we changed this to decent vs amazing hands because we could not implement non binary labels.
+                int i = 0;
+                foreach (var item in split)
+                {
+                    if (i != 10)
+                    {
+                        attri.Add(Convert.ToInt32(item));
+                        i++;
+                    }
+                }
+                fullData.Add(new Element(clas, attri));
+            }
+
+            file.Close();
+            return fullData;
+        }
+
+        private static List<Element> parseVote()
+        {
+            List<Element> fullData = new List<Element>();
+            string line;
+
+            // Read the file and display it line by line.
+            StreamReader file = new StreamReader("vote.data");
+            while ((line = file.ReadLine()) != null)
+            {
+                int clas;
+                List<int> attri = new List<int>();
+                line = line.Trim();
+                var split = line.Split(',');
+
+                // 0 if a democrat and 1 if republican
+                clas = split.First().Equals("democrat") ? 0 : 1;
+                int i = 0;
+                foreach (var item in split)
+                {
+                    if (i == 0)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        switch (item.ToString())
+                        {
+                            case "y":
+                                attri.Add(0);
+                                break;
+                            case "n":
+                                attri.Add(1);
+                                break;
+                            default:
+                                attri.Add(-1);
+                                break;
+                        }
+                    }
+                }
+                fullData.Add(new Element(clas, attri));
+            }
+
+            file.Close();
+            return fullData;
+        }
+
+        public static List<Element> parseData(int selectData)
+        {
+            switch (selectData)
+            {
+                case 1:
+                    return parseMonk();
+                case 2:
+                    return parseVote();
+                case 3:
+                    return parsePoker();
+
+            }
+            return null;
+
+        }
+    }
+}
---
+++ Homework2/hw2ann/hw2ann/backprop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hw2ann;
+
+namespace hw2ann
+{
+    class Backprop
+    {
+        public static void Main(string[] args)
+        {
+            List<Element> derp = Parser.parseData(1);
+            Console.ReadKey();
+        }
+    }
+}
